14ca6e6f8662f3ed215658fe9b9d2233ec9dc8f4
Liliya Taran <liliyataran2017@gmail.com>
2020-01-16 19:53:00 +0200

    Updates

---
+++ static/js/main.f03515ce.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonptask_tagsoft=this.webpackJsonptask_tagsoft||[]).push([[0],{3:function(t,e,n){t.exports=n(9)},8:function(t,e,n){},9:function(t,e,n){"use strict";n.r(e);var a=n(0),s=n.n(a),o=n(2),r=n.n(o);n(8);var c=function(){return s.a.createElement("div",{className:"App"},s.a.createElement("h1",null,"Hello!"))};r.a.render(s.a.createElement(c,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.f03515ce.chunk.js.map
